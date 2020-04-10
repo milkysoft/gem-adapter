cd244a09284f479fb10846370fe5234aed3423df
Sammers21 <titantins@gmail.com>
2020-04-10 18:34:31 +0300

    Move implementation todo from java to ruby

--- src/main/ruby/SubmitGem.rb
+++ src/main/ruby/SubmitGem.rb
@@ -1,4 +1,7 @@
 require 'java'
+# @todo #32:120min Gem submission implementation.
+#  The implementation must receive the .gem file, unzip it, and update specs files. As a result,
+#  files become available for downloading.
 class SubmitGem
   include com.artipie.http.Slice
   def response(line, headers, body)
