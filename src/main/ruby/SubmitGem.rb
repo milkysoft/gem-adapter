f86d3a3a22689ed2a7a191f9351d3939abbf75e1
Sammers21 <titantins@gmail.com>
2020-04-10 18:17:53 +0300

    fix static enumeration ref

--- src/main/ruby/SubmitGem.rb
+++ src/main/ruby/SubmitGem.rb
@@ -2,6 +2,6 @@ require 'java'
 class SubmitGem
   include com.artipie.http.Slice
   def response(line, headers, body)
-    com.artipie.http.rs.RsWithStatus.new(com.artipie.http.rs.RsStatus.NOT_IMPLEMENTED)
+    com.artipie.http.rs.RsWithStatus.new(com.artipie.http.rs.RsStatus::NOT_IMPLEMENTED)
   end
 end
