304f381b9389730a3720efda99a1b063805a0d5f
Sammers21 <titantins@gmail.com>
2020-04-17 06:33:44 +0300

    execute ruby_gen script on clean phase of lifecycle

--- ruby_lib.sh
+++ ruby_lib.sh
@@ -1,8 +1,9 @@
-rm -rf lib
-gem fetch builder
-BUILDER_GEM_FILE=$(ls -la | grep builder*\.gem | awk '{print $NF}')
-GEM_PATH=lib GEM_HOME=lib ruby -S gem install ./$BUILDER_GEM_FILE
-cd lib && jar -cf lib.jar .
-# remove only dirs
-rm -R `ls -1 -d */`
-cd .. && rm $BUILDER_GEM_FILE
+set -e
+set -x
+if [ ! -d "./lib" ]
+then
+  gem fetch builder
+  BUILDER_GEM_FILE=$(ls -la | grep builder*\.gem | awk '{print $NF}')
+  GEM_PATH=lib GEM_HOME=lib ruby -S gem install ./$BUILDER_GEM_FILE
+  rm $BUILDER_GEM_FILE
+fi
