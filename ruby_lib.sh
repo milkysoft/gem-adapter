ad1227b44dea0e4ac40dfee05708a5d683d17a9e
Sammers21 <titantins@gmail.com>
2020-04-17 09:07:32 +0300

    remove -la option

--- ruby_lib.sh
+++ ruby_lib.sh
@@ -3,7 +3,7 @@ set -x
 if [ ! -d "./lib" ]
 then
   gem fetch builder
-  BUILDER_GEM_FILE=$(ls -la | grep builder*\.gem | awk '{print $NF}')
+  BUILDER_GEM_FILE=$(ls | grep builder*\.gem | awk '{print $NF}')
   GEM_PATH=lib GEM_HOME=lib ruby -S gem install ./$BUILDER_GEM_FILE
   rm $BUILDER_GEM_FILE
 fi
