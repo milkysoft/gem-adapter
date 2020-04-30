6ca9688291f75e618769c0a945cfd13b9e203fd5
Kirill <g4s8.public@gmail.com>
2020-04-30 10:02:26 +0300

    #3 - fetch nodocs

--- ruby_lib.sh
+++ ruby_lib.sh
@@ -2,7 +2,7 @@ set -e
 set -x
 if [ ! -d "./lib" ]
 then
-  gem fetch builder
+  gem fetch --no-document builder
   BUILDER_GEM_FILE=$(ls | grep builder*\.gem | awk '{print $NF}')
   GEM_PATH=lib GEM_HOME=lib ruby -S gem install ./$BUILDER_GEM_FILE
   rm $BUILDER_GEM_FILE
