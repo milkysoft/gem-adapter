b1e2627f744524d1d4e92bd930b584f86e515899
Sammers21 <titantins@gmail.com>
2020-04-30 11:08:07 +0300

    dont use rdoc

--- ruby_lib.sh
+++ ruby_lib.sh
@@ -4,6 +4,6 @@ if [ ! -d "./lib" ]
 then
   gem fetch builder
   BUILDER_GEM_FILE=$(ls | grep builder*\.gem | awk '{print $NF}')
-  GEM_PATH=lib GEM_HOME=lib ruby -S gem install ./$BUILDER_GEM_FILE
+  GEM_PATH=lib GEM_HOME=lib ruby -S gem install ./$BUILDER_GEM_FILE --no-document --no-rdoc
   rm $BUILDER_GEM_FILE
 fi
