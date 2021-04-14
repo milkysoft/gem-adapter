befb5b7fa6cb0ed6380de392979a7a22ff317291
ALEXEY ZORCHENKOV <zuoqinrb@163.com>
2021-04-14 07:02:32 +0000

    Genrate Marshal

--- src/test/java/com/artipie/gem/RubyIndexerTest.java
+++ src/test/java/com/artipie/gem/RubyIndexerTest.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
 import org.apache.commons.io.IOUtils;
@@ -19,12 +20,13 @@ class RubyIndexerTest {
 
     @Test
     public void testUpdater(@TempDir Path tmp) throws Exception{
-        Path target = tmp.resolve("builder-3.2.4.gem");
+        Path path = Paths.get(tmp.toFile().getAbsolutePath(), "/gems");
+        Files.createDirectories(path);
+        Path target = path.resolve("builder-3.2.4.gem");
         try (InputStream is = this.getClass().getResourceAsStream("/builder-3.2.4.gem");
              OutputStream os = Files.newOutputStream(target)) {
             IOUtils.copy(is, os);
         }
-        // Paths.get("/mnt/disk2/projects/gem-adapter/" + repo)
         RubyIndexer rubyIndexer = new RubyIndexer(JavaEmbedUtils.initialize(new ArrayList<>(0)));
         rubyIndexer.index(tmp.toString());
     }
