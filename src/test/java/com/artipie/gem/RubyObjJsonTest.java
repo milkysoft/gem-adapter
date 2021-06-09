8115a1ae3224eb872c4f8e31db9451f3f4c63381
milkysoft <zuoqinrb@163.com>
2021-06-09 11:27:56 +0300

    GemInfo slice implemented
    For #85 - implemented GemInfo slice:
     - Import rubygems/commands/contents_command.rb and use ContentsCommand to obtain gem information
     - Added test to obtain information for built in did_you_mean ruby gem
    
    PR: #94

---
+++ src/test/java/com/artipie/gem/RubyObjJsonTest.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.gem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import org.apache.commons.io.IOUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.jruby.javasupport.JavaEmbedUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import wtf.g4s8.hamcrest.json.JsonHas;
+import wtf.g4s8.hamcrest.json.JsonValueIs;
+
+/**
+ * A test for extract JSON info from gem .
+ *
+ * @since 1.0
+ */
+public class RubyObjJsonTest {
+
+    @Test
+    public void createJsonByPath(@TempDir final Path tmp) throws IOException {
+        final String builderstr = "gviz-0.3.5.gem";
+        final String gemattr = "homepage";
+        final String attrval = "https://github.com/melborne/Gviz";
+        final Path target = tmp.resolve(builderstr);
+        try (InputStream is = this.getClass().getResourceAsStream("/".concat(builderstr));
+            OutputStream os = Files.newOutputStream(target)) {
+            IOUtils.copy(is, os);
+        }
+        MatcherAssert.assertThat(
+            new RubyObjJson(
+                JavaEmbedUtils.newRuntimeAdapter(),
+                JavaEmbedUtils.initialize(Collections.emptyList())
+            ).createJson(Paths.get(tmp.toString(), builderstr)),
+            Matchers.allOf(
+                new JsonHas(
+                    gemattr,
+                    new JsonValueIs(attrval)
+                )
+            )
+        );
+    }
+}
