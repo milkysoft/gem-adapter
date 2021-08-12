0b47760f92a1abe07dc7148f34c2e68d89cf04b9
ALEXEY ZORCHENKOV <zuoqinrb@163.com>
2021-08-12 11:36:45 +0300

    Issue #114 fix github comments

--- src/main/ruby/updatemeta.rb
+++ src/main/ruby/updatemeta.rb
@@ -10,51 +10,49 @@ class Ex
     end
 
     def info()
-        begin
-            spec = Gem::Package.new(@val).spec()
-            found = false
-            metas = []
-            f = 'latest_specs.4.8'
-            if File.file?(f) and File.size(f) > 10
-                content = File.open(f).read
-                metas = Marshal.load(content)
-                metas.each do |item|
-                    if item[0] == spec.name && item[1].version == spec.version.version
-                        found = true
-                    end
+        spec = Gem::Package.new(@val).spec()
+        found = false
+        metas = []
+        f = 'latest_specs.4.8'
+        if File.file?(f) and File.size(f) > 10
+            content = File.open(f).read
+            metas = Marshal.load(content)
+            metas.each do |item|
+                if item[0] == spec.name && item[1].version == spec.version.version
+                    found = true
                 end
             end
-            if found == false
-                metas.push([spec.name, Gem::Version.create(spec.version.version), "ruby"])
-                data = Marshal.dump(metas)
-                File.write(f, data)
-                Zlib::GzipWriter.open(f + '.gz') do |gz|
-                    gz.write data
-                end
+        end
+        if found == false
+            metas.push([spec.name, Gem::Version.create(spec.version.version), "ruby"])
+            data = Marshal.dump(metas)
+            File.write(f, data)
+            Zlib::GzipWriter.open(f + '.gz') do |gz|
+                gz.write data
             end
+        end
 
-            found = false
-            metas = []
-       	    f =	'specs.4.8'
-            if File.file?(f) and File.size(f) > 10
-                content = File.open(f).read
-                metas = Marshal.load(content)
-                metas.each do |item|
-                    if item[0] == spec.name && item[1].version == spec.version.version
-                        found = true
-                    end
+        found = false
+        metas = []
+        f =	'specs.4.8'
+        if File.file?(f) and File.size(f) > 10
+            content = File.open(f).read
+            metas = Marshal.load(content)
+            metas.each do |item|
+                if item[0] == spec.name && item[1].version == spec.version.version
+                    found = true
                 end
             end
-            if found == false
-                metas.push([spec.name, Gem::Version.create(spec.version.version), "ruby"])
-                data = Marshal.dump(metas)
-                File.write(f, data)
-                Zlib::GzipWriter.open(f + '.gz') do |gz|
-                    gz.write data
-                end
+        end
+        if found == false
+            metas.push([spec.name, Gem::Version.create(spec.version.version), "ruby"])
+            data = Marshal.dump(metas)
+            File.write(f, data)
+            Zlib::GzipWriter.open(f + '.gz') do |gz|
+                gz.write data
             end
+        end
         rescue Exception => e
             puts e
         end
-    end
 end
